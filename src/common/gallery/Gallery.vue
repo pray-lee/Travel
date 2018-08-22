206f65d71f9c3956983d4efb42696121e1f71773
pray-lee <praycis@gmail.com>
2018-08-22 14:10:53 +0800

    gallary and detail pages

---
+++ src/common/gallery/Gallery.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="container" @click="handleGallaryClick">
+    <div class="wrapper">
+      <swiper :options="swiperOption">
+        <!-- slides -->
+        <swiper-slide v-for="img of imgs">
+          <img class="swiper-img"
+               :src="img"
+               alt=""/>
+        </swiper-slide>
+        <!-- Optional controls -->
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CommonGallery',
+    props: {
+      imgs: {
+        type: Array,
+        default () {
+          return [
+            'http://img1.qunarzz.com/sight/p0/1603/60/6020660bc5fc5c7d90.water.jpg_r_800x800_910172f1.jpg',
+            'http://img1.qunarzz.com/sight/p0/1603/60/6020660bc5fc5c7d90.water.jpg_r_800x800_910172f1.jpg'
+          ]
+        }
+      }
+    },
+    data () {
+      return {
+        swiperOption: {
+          pagination: '.swiper-pagination',
+          paginationType: 'fraction',
+          loop: true,
+          observeParents: true,
+          observer: true
+        }
+      }
+    },
+    methods: {
+      handleGallaryClick () {
+        this.$emit('close')
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .container >>> .swiper-container
+    overflow:inherit
+  .container
+    display: flex
+    flex-direction: column
+    justify-content: center
+    z-index: 999
+    position: fixed
+    top: 0
+    right: 0
+    left: 0
+    bottom: 0
+    background: #000
+    .wrapper
+      background: #fff
+      text-align: center
+      .swiper-pagination
+        color: #fff
+        bottom: -1rem
+      .swiper-img
+        width:100%
+</style>
---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="https://img1.qunarzz.com/sight/p0/201311/12/3e2de7e7f2fa170fc8d65eac.jpg_600x330_c73533f8.jpg" alt="">
+      <div class="banner-info">
+        <div class="banner-title">
+          测试标题
+        </div>
+        <div class="banner-number">
+            <span class="iconfont banner-icon">&#xe632;</span>
+            49
+        </div>
+      </div>
+    </div>
+    <common-gallery :imgs="imgs" v-show="showGallary" @close="handleGallaryClose"></common-gallery>
+  </div>
+
+</template>
+<script>
+  import CommonGallery from 'common/gallery/Gallery'
+  export default {
+    name: 'DetailBanner',
+    components: {
+      CommonGallery
+    },
+    computed: {
+
+    },
+    methods: {
+      handleBannerClick () {
+        this.showGallary = true
+      },
+      handleGallaryClose () {
+        this.showGallary = false
+      }
+    },
+    data () {
+      return {
+        showGallary: false,
+        imgs: [
+          'http://img1.qunarzz.com/sight/p0/1603/60/6020660bc5fc5c7d90.water.jpg_r_800x800_910172f1.jpg',
+          'http://img1.qunarzz.com/sight/p0/1603/60/6020660bc5fc5c7d90.water.jpg_r_800x800_910172f1.jpg',
+          'http://img1.qunarzz.com/sight/p0/1603/60/6020660bc5fc5c7d90.water.jpg_r_800x800_910172f1.jpg'
+        ]
+      }
+    }
+  }
+</script>
+<style lang="stylus" scoped>
+  .banner
+    position:relative
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    .banner-img
+      width: 100%
+    .banner-info
+      display: flex
+      position: absolute
+      left:0
+      right:0
+      bottom:0
+      line-height: .6rem
+      color: #fff
+      .banner-title
+        flex:1
+        font-size: .32rem
+        padding: 0 .2rem
+      .banner-number
+        margin-top: .14rem
+        padding: 0 .4rem
+        height: .32rem
+        line-height: .32rem
+        border-radius: .2rem
+        background: rgba(0,0,0,.6)
+        font-size: .24rem
+        .banner-icon
+          font-size: .24rem
+</style>
